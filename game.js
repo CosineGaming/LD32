be5205ff1209ab44b2cae77672c9122985b532e6
CosineP <judahiii@gmail.com>
2015-04-18 10:15:04 -0400

    Switching computers
    What I did
     - Nearest neighbor
     - Gravity and physics!
      - Wall jumping!

--- game.js
+++ game.js
@@ -9,17 +9,23 @@ var game;
 var world = {};
 var sounds = {};
 
-var camera = [0, 0];
-
 var keys = new Array(256);
 
 var lastTime = 0;
 
-var splits = 0;
 var quadrantSpeeds = [];
 
 var levels;
 var currentLevel = 0;
+
+var tileWidth = 32;
+var tileHeight = 16;
+
+var gravity = 0.3;
+var velocity = 0;
+var xVelocity = 0;
+
+var lastSpace = false;
 
 // Entry point
 
@@ -34,17 +40,9 @@ function initialize()
 	if (!container.getContext || !container.getContext("2d"))
 	{
 		unsupported();
-		return;
+		return false;
 	}
 	game = container.getContext("2d");
-
-	fillBrowser();
-	window.onresize = fillBrowser;
-
-	container.mozImageSmoothingEnabled = false;
-	container.webkitImageSmoothingEnabled = false;
-	container.msImageSmoothingEnabled = false;
-	container.imageSmoothingEnabled = false;
 
 	container.setAttribute("tabindex", "0");
 	container.focus();
@@ -56,6 +54,11 @@ function initialize()
 
 	initializeWorld();
 
+	container.width = tileWidth * levels[0][0].length;
+	container.height = tileHeight * levels[0].length;
+
+	window.requestAnimationFrame(update);
+
 	return true;
 
 }
@@ -66,13 +69,24 @@ function initializeWorld()
 	levels = [
 
 		[
-			"1 1 1 1 1 1 1 1 1 1 1 1 1 1",
-			"1 - - - - - - - - - - - - 1",
-			"1 - - - - - - - - - - - - 1",
-			"1 - - - - - - - - - - - - 1",
-			"1 - - - - - - - - - - - - 1",
-			"1 - - - - - - - - - - - - 1",
-			"1 1 1 1 1 1 1 1 1 1 1 1 1 1",
+			"- - - - - - - - - - - - - - - - - -",
+			"- - - - - - - - - - - - - - - - - -",
+			"- - - - - - - - - - - - - - - - - -",
+			"- - - - - - - - - - - - - - - - - -",
+			"- - - - - - - 1 - - - - - - - - - -",
+			"- - - - - - - 1 - - - - - - - - - -",
+			"- - - - - - - 1 - - - - - - - - - -",
+			"- - - - - - - 1 - - - - - - - - - -",
+			"- - - - - - - 1 - - - - - - - - - -",
+			"- - - - - - - 1 - - - - - - - - - -",
+			"- - - - - - - - - - - - - - - - - -",
+			"- - - - 1 - - - - - - - 1 - - - - -",
+			"- - - - 1 - - - - - - - - - - 1 - -",
+			"- - - - - - - - - - - 1 1 - - - - -",
+			"- 1 - - - - - - - - - 1 1 - - - - -",
+			"- - - - - - - - - - - - - - - 1 - -",
+			"- - - - - - - - - - - - - - - 1 - -",
+			"- - 1 1 1 - - 1 1 - 1 - 1 - - 1 - -"
 		]
 
 	];
@@ -95,7 +109,14 @@ function initializeWorld()
 	world["1"] = new Object();
 	loadImage(world["1"], "assets/test.png");
 
-	window.requestAnimationFrame(update);
+	world["player"] = new Object();
+	world["player"].x = 50;
+	world["player"].y = 50;
+	world["player"].speed = 6;
+	world["player"].quadrant = [0, 0];
+	loadImage(world["player"], "assets/bob.png");
+
+	resetQuadrantSpeeds();
 
 }
 
@@ -122,66 +143,18 @@ function render(updateTime)
 			{
 				if (tile.image)
 				{
-					game.drawImage(tile.image,
-						0, 0, 16, 16,
-						x * container.width / levels[currentLevel][y].length,
-						y * container.height / levels[currentLevel].length,
-						container.width / levels[currentLevel][y].length,
-						container.height / levels[currentLevel].length);
+					game.drawImage(tile.image, Math.round(x * tileWidth), Math.round(y * tileHeight));
 				}
 			}
 		}
 	}
 
-	/*var entitiesByLayer = [];
-
-	// Order the entities by layer so we draw them correctly
-	everyEntity(
-		function(entity)
-		{
-			// Only render entities with images and positions (aka they're set to be rendered)
-			if (entity.image && entity.position)
-			{
-				if (!entity.layer)
-				{
-					entity.layer = 0;
-				}
-				if (entity.z)
-				{
-					entity.layer = entity.z;
-				}
-				for (var i=0; i<entitiesByLayer.length; ++i)
-				{
-					if (entitiesByLayer[i].layer >= entity.layer)
-					{
-						entitiesByLayer.splice(i, 0, entity);
-						debugLog(entity.x);
-						// Return from function(entity), not render
-						return;
-					}
-				}
-				entitiesByLayer.push(entity);
-			}
-		}
-	);
-
-	// Render entities that weren't behind anything and can be simply drawn last
-	for (var i=0; i<entitiesByLayer.length; ++i)
-	{
-		var entity = entitiesByLayer[i];
-		if (entity.image && entity.position)
-		{
-			if (!entity.screenRelative)
-			{
-				x -= camera[0];
-				y -= camera[1];
-			}
-			game.drawImage(entity.image, x, y);
-		}
-	}*/
-
-	everyEntity(function(){
+	everyEntity(function(entity){
 		// Code
+		if (entity.image)
+		{
+			game.drawImage(entity.image, Math.round(entity.x), Math.round(entity.y));
+		}
 	});
 
 	var displayFps = true;
@@ -191,8 +164,8 @@ function render(updateTime)
 		game.font = "20px white Candara";
 		game.strokeStyle = "#FFF";
 		game.fillStyle = "#000"
-		game.fillText(String(Math.round(updateTime)), 30, 30);
-		game.strokeText(String(Math.round(updateTime)), 30, 30);
+		game.fillText(String(Math.round(updateTime)), 20, 20);
+		game.strokeText(String(Math.round(updateTime)), 20, 20);
 	}
 
 }
@@ -205,6 +178,94 @@ function update(totalTime)
 	delta = totalTime - lastTime;
 
 	// Code
+	var player = world["player"];
+
+	if (player.image)
+	{
+
+		var speedMod = quadrantSpeed(getQuadrant(player.x, player.y));
+		var speed = player.speed * speedMod;
+
+		var oldX = player.x;
+		var oldY = player.y;
+
+		var hangLeft = collidesTile(player, player.x + 1, player.y - 1);
+		var hangRight = collidesTile(player, player.x - 1, player.y - 1);
+
+		if (key("A") && !hangLeft)
+		{
+			player.x -= speed;
+		}
+		if (key("DE") && !hangRight)
+		{
+			player.x += speed;
+		}
+		var jumpSpeed = -5;
+		if (key(" W") && collidesTile(player, player.x, player.y) && !lastSpace)
+		{
+			velocity = jumpSpeed;
+		}
+
+		velocity += gravity * speedMod;
+		if (hangLeft || hangRight)
+		{
+			velocity = 0.75;
+		}
+		player.y += velocity * speedMod;
+
+		xVelocity *= 0.65;
+		player.x += xVelocity
+
+		var goingRight = oldX < player.x;
+		var goingLeft = oldX > player.x;
+		var byX = collidesTile(player, player.x, oldY - 1);
+		var byY = collidesTile(player, oldX, player.y);
+		var goingDown = oldY < player.y;
+
+		if (collidesTile(player, player.x, player.y))
+		{
+
+			if (byY)
+			{
+				velocity = 0;
+				player.y = Math.floor((player.y + player.image.height * goingDown) / tileHeight)
+					* tileHeight - player.image.height * goingDown + tileHeight * !goingDown;
+			}
+			if (byX)
+			{
+				player.x = Math.floor((player.x + player.image.width * goingRight) / tileWidth)
+					* tileWidth - player.image.width * goingRight + tileWidth * goingLeft;
+			}
+
+		}
+
+		if ((hangLeft || hangRight) && key(" ") && !lastSpace)
+		{
+			velocity = jumpSpeed;
+			xVelocity = 10 * (hangRight * 2 - 1);
+			debugLog(xVelocity);
+			player.x += speed * (hangRight * 2 - 1);
+		}
+
+		lastSpace = key(" W");
+
+		if (player.x < 0)
+		{
+			player.x = 0;
+		}
+		if (player.x > container.width - player.image.width)
+		{
+			player.x = container.width - player.image.width;
+		}
+		if (player.y < 0)
+		{
+			player.y = 0;
+		}
+		if (player.y > container.height - player.image.height)
+		{
+			// Die.
+		}
+	}
 
 	render(delta);
 
@@ -237,12 +298,6 @@ function everyEntity(what)
 	}
 }
 
-function fillBrowser(event)
-{
-	container.width = window.innerWidth;
-	container.height = window.innerHeight;
-}
-
 function unsupported()
 {
 	document.getElementById("error-text").innerHTML = "Sorry, but you're using an unsupported browser and can't play this game.";
@@ -264,17 +319,63 @@ function debugLog(log)
 
 function key(which)
 {
-	return keys[which.charCodeAt(0)]
+	for (var char=0; char<which.length; char++)
+	{
+		if (keys[which.charCodeAt(char)])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+function tilePos(x, y)
+{
+	return [Math.floor(x / tileWidth), Math.floor(y / tileHeight)];
+}
+
+function getTile(tile)
+{
+	return levels[currentLevel][tile[1]][tile[0]]
 }
 
 function getQuadrant(x, y)
 {
-	return [int(x * (splits + 1) / container.width)]
+	return [Math.floor(x * quadrantSpeeds.length / container.width),
+		Math.floor(y * quadrantSpeeds.length / container.height)];
 }
 
-function changedQuadrant(object)
+function fourCorners(object, x, y, testFunction)
 {
 	for (var plusWidth=0; plusWidth<2; plusWidth++)
+	{
+		for (var plusHeight=0; plusHeight<2; plusHeight++)
+		{
+			var testValue = testFunction(object, x + (object.image.width) * plusWidth,
+				y + (object.image.height) * plusHeight);
+			if (testValue)
+			{
+				return testValue;
+			}
+		}
+	}
+	return false;
+}
+
+function changedQuadrant(object, x, y)
+{
+	return fourCorners(object, x, y, function(test, x, y){
+		var quadrant = getQuadrant(x, y);
+		if (quadrant == test.quadrant)
+		{
+			return false;
+		}
+		else
+		{
+			return quadrant;
+		}
+	});
+	/*for (var plusWidth=0; plusWidth<2; plusWidth++)
 	{
 		for (var plusHeight=0; plusHeight<2; plusHeight++)
 		{
@@ -286,7 +387,7 @@ function changedQuadrant(object)
 			}
 		}
 	}
-	return false;
+	return false;*/
 }
 
 function quadrantSpeed(quadrant)
@@ -297,12 +398,12 @@ function quadrantSpeed(quadrant)
 function resetQuadrantSpeeds()
 {
 	quadrantSpeeds = [];
-	for (var x=0; x<splits; ++x)
+	for (var x=0; x<currentLevel+2; ++x)
 	{
-		quadrantSpeeds.append([]);
-		for (var y=0; y<splits; ++y)
+		quadrantSpeeds.push([]);
+		for (var y=0; y<currentLevel+2; ++y)
 		{
-			quadrantSpeeds[x].append(1);
+			quadrantSpeeds[x].push(1);
 		}
 	}
 }
@@ -316,12 +417,47 @@ function updateQuadrant(object)
 	}
 }
 
+function collidesTile(object, x, y)
+{
+	var topLeft = tilePos(x, y);
+	var bottomRight = tilePos(x + object.image.width - 1, y + object.image.height);
+	for (var x=topLeft[0]; x<bottomRight[0] + 1; x++)
+	{
+		for (var y=topLeft[1]; y<bottomRight[1] + 1; y++)
+		{
+			if (getTile([x, y]) != "-")
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 // Input Handling
 
 function mouseDown(event)
 {
-	var x = event.clientX + camera[0];
-	var y = event.clientY + camera[1];
+
+	var x = container.width / window.innerWidth * event.clientX;
+	var y = container.height / window.innerHeight * event.clientY;
+	var quadrant = getQuadrant(x, y);
+
+	var speed = quadrantSpeed(quadrant);
+
+	if (speed != 0)
+	{
+		quadrantSpeeds[quadrant[0]][quadrant[1]] *= 2;
+	}
+	else
+	{
+		quadrantSpeeds[quadrant[0]][quadrant[1]] = 0.5;
+	}
+	if (speed == 2)
+	{
+		quadrantSpeeds[quadrant[0]][quadrant[1]] = 0;
+	}
+
 }
 
 function mouseUp(event)
@@ -339,7 +475,6 @@ function keyDown(event)
 	if (!keys[event.keyCode])
 	{
 		// Don't act on repeats:
-
 	}
 	// Do act on repeats:
 	keys[event.keyCode] = true;
